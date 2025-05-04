8823e3d05724e7c3bbf2e308e9ad110d3e57aac0
jakeogh <github.com@v6y.net>
2025-05-03 19:26:29 -0700

    auto-commit

--- mounttool/__init__.py
+++ mounttool/__init__.py
@@ -1,4 +1,8 @@
-#from .mounttool import mounttool
-from .mounttool import block_special_path_is_mounted
-from .mounttool import mount_something
-from .mounttool import path_is_mounted
+"""
+isort:skip_file
+"""
+
+# from .mounttool import mounttool
+from .mounttool import block_special_path_is_mounted as block_special_path_is_mounted
+from .mounttool import mount_something as mount_something
+from .mounttool import path_is_mounted as path_is_mounted
