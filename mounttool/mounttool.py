3a7dc80f45dbe175bc11de23851f3b6efa29633e
jakeogh <github.com@v6y.net>
2026-07-21 17:21:33 -0700

    update

--- mounttool/mounttool.py
+++ mounttool/mounttool.py
@@ -1,36 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
-# pylint: disable=missing-docstring               # [C0111] docstrings are always outdated and wrong
-# pylint: disable=fixme                           # [W0511] todo is encouraged
-# pylint: disable=line-too-long                   # [C0301]
-# pylint: disable=too-many-instance-attributes    # [R0902]
-# pylint: disable=too-many-lines                  # [C0302] too many lines in module
-# pylint: disable=invalid-name                    # [C0103] single letter var names, name too descriptive
-# pylint: disable=too-many-return-statements      # [R0911]
-# pylint: disable=too-many-branches               # [R0912]
-# pylint: disable=too-many-statements             # [R0915]
-# pylint: disable=too-many-arguments              # [R0913]
-# pylint: disable=too-many-nested-blocks          # [R1702]
-# pylint: disable=too-many-locals                 # [R0914]
-# pylint: disable=too-few-public-methods          # [R0903]
-# pylint: disable=no-member                       # [E1101] no member for base
-# pylint: disable=attribute-defined-outside-init  # [W0201]
-# pylint: disable=too-many-boolean-expressions    # [R0916] in if statement
 from __future__ import annotations
 
 import os
 from pathlib import Path
 
-# import click
 import sh
 from asserttool import ic
-# from asserttool import icp
-# from click_auto_help import AHGroup
-# from clicktool import click_add_options
-# from clicktool import click_global_options
-# from clicktool import tvicgvd
-# from globalverbose import gvd
 from pathtool import path_is_block_special
 from psutil import disk_partitions
 
@@ -97,9 +74,19 @@ def mount_something(
     slave_command = None
     mount_command = None
     if mount_type == "proc":
-        mount_command = sh.mount.bake("-t", "proc", "none", mountpoint)
+        mount_command = sh.mount.bake(
+            "-t",
+            "proc",
+            "none",
+            mountpoint,
+        )
     elif mount_type == "tmpfs":
-        mount_command = sh.mount.bake("-t", "tmpfs", "none", mountpoint)
+        mount_command = sh.mount.bake(
+            "-t",
+            "tmpfs",
+            "none",
+            mountpoint,
+        )
     elif mount_type == "bind":
         if source:
             mount_command = sh.mount.bake("--bind", source.as_posix(), mountpoint)
